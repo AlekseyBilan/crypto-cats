b23bbb44ac7858381c5db9e9d234d6a45c05324c
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2018-12-02 22:39:26 +0200

    fix bug on CatList component

--- src/CatsList.js
+++ src/CatsList.js
@@ -8,23 +8,26 @@ export default class CatsList extends Component {
     render() {
         let {index, prevCatGeneration, cat} = this.props;
         return <Fragment>
-                    {(prevCatGeneration !== cat.generation) ? (
+                    ( prevCatGeneration === cat.generation ) ? (
                         <tr key={index + cat.generation}>
                             <td className="generation" colSpan={3}>
                                 Generation {cat.generation}
                             </td>
                         </tr>
-                    ) : null }
-                    <tr key={cat.id}>
+                    <Fragment>
+                        <tr key={cat.id}>
                         <td key={cat.id}>
                         <Avatar src = {cat.image_url}/>
-                        {cat.name || 'a cat'}
+                    {   cat.name || 'a cat'}
                         </td>
                         <td>{Number(cat.auction.start_price.slice(0, 5)).toFixed(2)} $</td>
                         <td className="buy-btn-wrap">
                         <button>Buy</button>
                         </td>
-                    </tr>
+                        </tr>
+                    </Fragment>
+                    ) : null
                 </Fragment>
+
     }
 }
